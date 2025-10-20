e39755b1c4ee1199e499a79015c9c26d9b8eeb5e
SebasCadena <juan.cadena02@uceva.edu.co>
2025-10-20 10:12:29 -0500

    feat: Integrate Firebase services and update app icons with new assets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.gms.google-services")
+}
+
+dependencies{
+    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
 
 android {
