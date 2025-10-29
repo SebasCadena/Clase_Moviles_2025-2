756b9567fd2f7513b720dd99669ee87d51742d74
SebasCadena <juan.cadena02@uceva.edu.co>
2025-10-29 16:07:43 -0500

    feat(auth): Implement user authentication service and login UI

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -5,7 +5,8 @@ import 'package:talleres/views/about/aboutme.dart';
 import 'package:talleres/views/home/home.dart';
 import 'package:talleres/views/http_API/api.dart';
 import 'package:talleres/views/http_API/detalle.dart';
-import 'package:talleres/views/login/login.dart';
+// import 'package:talleres/views/login/login.dart';
+import 'package:talleres/views/auth/login.dart';
 import 'package:talleres/views/TabBar/bar.dart';
 import 'package:talleres/views/SegundoPlano/usuarios.dart';
 
